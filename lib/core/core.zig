2161bb8ce3a879eb589d5dc597c5ff87a5aa41f0
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 11:54:56 +0100

    slice 25: add core membership modules

--- lib/core/core.zig
+++ lib/core/core.zig
@@ -1,5 +1,11 @@
+pub const membership = @import("membership.zig");
+pub const route_table = @import("route_table.zig");
+pub const policy = @import("policy.zig");
 pub const types = @import("types.zig");
 
 test {
+    _ = membership;
+    _ = route_table;
+    _ = policy;
     _ = types;
 }
