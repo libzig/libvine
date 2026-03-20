4b7c42e1c4b8cc50ad7e928242c492aec7d37c16
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 12:38:45 +0100

    slice 87: add forwarding engine

--- lib/core/forwarder.zig
+++ lib/core/forwarder.zig
@@ -1,1 +1,9 @@
-pub const placeholder = struct {};
+const linux = @import("../linux/linux.zig");
+const route_table = @import("route_table.zig");
+const session_table = @import("session_table.zig");
+
+pub const Forwarder = struct {
+    routes: *route_table.RouteTable,
+    sessions: *session_table.SessionTable,
+    tun: *linux.tun.TunDevice,
+};
