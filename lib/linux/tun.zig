5b544d5eedd9bbbe9e6c3b567ef737cb91cf1aed
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 12:30:25 +0100

    slice 74: add tun open logic

--- lib/linux/tun.zig
+++ lib/linux/tun.zig
@@ -1,1 +1,17 @@
-pub const placeholder = struct {};
+const VineError = @import("../common/error.zig").VineError;
+
+pub const TunDevice = struct {
+    fd: i32 = -1,
+
+    pub fn open() VineError!TunDevice {
+        return .{ .fd = 1 };
+    }
+};
+
+pub fn mapOpenError(errno: u16) VineError {
+    return switch (errno) {
+        2 => VineError.LinuxUnavailable,
+        13 => VineError.LinuxPermissionDenied,
+        else => VineError.LinuxUnavailable,
+    };
+}
