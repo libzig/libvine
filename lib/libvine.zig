7c428c5e86a750e51aa80c6792fe73bc6f22dade
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 11:35:54 +0100

    slice 3: add top-level module groups

--- lib/libvine.zig
+++ lib/libvine.zig
@@ -1,1 +1,19 @@
-pub fn placeholder() void {}
+pub const api = @import("api/api.zig");
+pub const common = @import("common/common.zig");
+pub const control = @import("control/control.zig");
+pub const core = @import("core/core.zig");
+pub const data = @import("data/data.zig");
+pub const integration = @import("integration/integration.zig");
+pub const linux = @import("linux/linux.zig");
+pub const testing = @import("testing/testing.zig");
+
+test {
+    _ = api;
+    _ = common;
+    _ = control;
+    _ = core;
+    _ = data;
+    _ = integration;
+    _ = linux;
+    _ = testing;
+}
