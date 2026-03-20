10d55f48856739540e93b5e96ff3bf1ccb08f941
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 12:31:03 +0100

    slice 75: add tun interface configuration

--- lib/linux/tun.zig
+++ lib/linux/tun.zig
@@ -2,10 +2,20 @@ const VineError = @import("../common/error.zig").VineError;
 
 pub const TunDevice = struct {
     fd: i32 = -1,
+    ifname: [16]u8 = [_]u8{0} ** 16,
 
     pub fn open() VineError!TunDevice {
         return .{ .fd = 1 };
     }
+
+    pub fn configure(self: *TunDevice, request: IfReq) void {
+        self.ifname = request.name;
+    }
+};
+
+pub const IfReq = struct {
+    name: [16]u8 = [_]u8{0} ** 16,
+    flags: u16 = 0,
 };
 
 pub fn mapOpenError(errno: u16) VineError {
