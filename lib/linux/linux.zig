0c3c73a4723406d2e5004b24d32c7925bfec3d3b
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 12:29:58 +0100

    slice 73: add linux module skeleton

--- lib/linux/linux.zig
+++ lib/linux/linux.zig
@@ -1,1 +1,9 @@
-pub const placeholder = struct {};
+pub const link_config = @import("link_config.zig");
+pub const routes = @import("routes.zig");
+pub const tun = @import("tun.zig");
+
+test {
+    _ = link_config;
+    _ = routes;
+    _ = tun;
+}
