b3f65e1a9b4d9c7945cbb38825248321e9cad72d
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 12:33:16 +0100

    slice 80: add link config module

--- lib/linux/link_config.zig
+++ lib/linux/link_config.zig
@@ -1,1 +1,8 @@
-pub const placeholder = struct {};
+const types = @import("../core/types.zig");
+
+pub const LinkState = struct {
+    ifname: [16]u8 = [_]u8{0} ** 16,
+    address: ?types.VineAddress = null,
+    prefix_len: ?u8 = null,
+    up: bool = false,
+};
