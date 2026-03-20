ca31e46e271d796242914df3900590743a98e88b
Trim Bresilla <trim.bresilla@gmail.com>
2026-03-20 12:37:55 +0100

    slice 85: add session and forwarder modules

--- lib/core/core.zig
+++ lib/core/core.zig
@@ -1,11 +1,15 @@
 pub const membership = @import("membership.zig");
+pub const forwarder = @import("forwarder.zig");
 pub const route_table = @import("route_table.zig");
+pub const session_table = @import("session_table.zig");
 pub const policy = @import("policy.zig");
 pub const types = @import("types.zig");
 
 test {
     _ = membership;
+    _ = forwarder;
     _ = route_table;
+    _ = session_table;
     _ = policy;
     _ = types;
 }
